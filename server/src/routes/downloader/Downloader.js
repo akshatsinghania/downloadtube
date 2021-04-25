ed19c09bfb1dd8430af5373c06d7b936db8e8bce
akshatsinghania <singhaniaakshat1@gmail.com>
2021-04-25 20:39:08 +0530

    DEPLOYED FRONTEND AND BACKEND

--- server/src/routes/downloader/Downloader.js
+++ server/src/routes/downloader/Downloader.js
@@ -4,5 +4,4 @@ const { getVideo } = require('../../controllers/downloader/Downloader');
 app.get('/:id', async (req, res) => {
 	await getVideo(`https://www.youtube.com/watch?v=${req.params.id}`, res);
 });
-
 module.exports = app;
